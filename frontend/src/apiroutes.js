1010167b21a8275f828491b2aa229c6259f5760c
abhiram050904 <142649536+abhiram050904@users.noreply.github.com>
2024-10-19 21:51:05 +0530

    Update apiroutes.js

--- frontend/src/apiroutes.js
+++ frontend/src/apiroutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "https://expense-tracker-x9wt.onrender.com";
 export const RegisterRoute = `${host}/api/v1/auth/register`;
 export const LoginRoute = `${host}/api/v1/auth/login`;
 export const LogoutRoute = `${host}/api/v1/auth/logout`;
